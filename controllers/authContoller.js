6a6605e0e9bb5b5416efd275a9887755f36dec41
santoshacharya07 <santoshacharya747@gamil.com>
2023-06-14 23:26:09 +0545

    working on register and loogin controller

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -1,7 +1,47 @@
+const User = require("../models/User");
+const CryptoJS= require("crypto-js");
 module.exports = {
-    updateUser: async (req, res) => {
+    createUser: async (req, res) => {
+const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    //CryptoJS.DES.encrypt(req.body.password,process.env.SECRET);
+    password: CryptoJS.DES.encrypt(req.body.password,process.env.SECRET).toString(),
+});
+try{
+const savedUser=await newUser.save();
+res.status(201).json(savedUser);
+}catch(error){
+res.status(500).json(error)
+}
 
-    },
+},
+
+//login user
+loginUser:async (req,res)=>{
+    try{
+const user =await User.findOne({email:req.body.email});
+!user && res.status(401).json("Wrong Login Details");
+
+const decryptedpass = CryptoJS.DES.decrypt(user.password,process.env.SECRET);
+const depassword=decryptedpass.toString(CryptoJS.enc.Utf8);
+depassword!==req.body.password && res.status(401).json("Wrong Password");
+
+const{password,__v, createdAt, ...others}=user._doc;
+res.status(200).json(others);
+    }catch(error){
+res.status(500);
+    }
+}
 
 
 }
+
+
+// module.exports = {
+//     updateUser: async (req, res) => {
+
+//     },
+
+
+// }
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
 const dotenv=require('dotenv');
+const mongoose = require('mongoose');
+// const { required } = require('nodemon/lib/config');
+const authRoute=require('./routes/auth');
+
+
 
 dotenv.config();
 //process.env.VARIABLE_NAME
 
-app.get('/', (req, res) => res.send('Hello Quantum!!'))
+mongoose.connect(process.env.MONGO_URL)
+    .then(()=>console.log('db conncted'))
+    .catch((err)=>{console.log(err)});
+
+// app.get('/', (req, res) => res.send('Hello Quantum!!'))
+app.use(express.json());
+
+app.use("/api/",authRoute);
+//localhost:5001/api/register
+
 app.listen(process.env.PORT || 5002, () => console.log(`Example app listening on port ${process.env.PORT }!`))
