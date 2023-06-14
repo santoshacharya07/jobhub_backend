5872eec3d04fe0c82ad1109d264621b4c0b9b37b
santoshacharya07 <santoshacharya747@gamil.com>
2023-06-14 13:57:28 +0545

    initail commmit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const dotenv=require('dotenv');
+
+dotenv.config();
+//process.env.VARIABLE_NAME
+
+app.get('/', (req, res) => res.send('Hello Quantum!!'))
+app.listen(process.env.PORT || 5002, () => console.log(`Example app listening on port ${process.env.PORT }!`))
